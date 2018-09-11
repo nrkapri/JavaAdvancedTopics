9866bd1c1d1554adaf618d3a8f00d759824fbfc2
Nayan Kapri <nayan.kapri@amdocs.com>
2018-09-11 10:06:20 +0530

    functional interface

---
+++ src/collection/CollectorsExample.java
@@ -0,0 +1,49 @@
+package collection;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import com.google.common.collect.ImmutableList;
+
+public class CollectorsExample {
+
+	public static void main(String[] args) {
+		List<String> givenList = Arrays.asList("a", "bb", "ccc", "dd");
+		
+		List<String> result = givenList.stream()
+				  .collect(toList());
+		System.out.println(result);
+		
+		List<String> result1 = givenList.stream()
+				  .collect(toCollection(LinkedList::new));
+		
+		System.out.println(result1);
+		
+		Map<String, Integer> result3 = givenList.stream()
+				  .collect(toMap(Function.identity(), String::length));
+		System.out.println(result3);
+		//Function.identity() is just a shortcut for defining function that accepts and return the same value;
+		
+		//handle duplicates
+		Map<String, Integer> result4 = givenList.stream()
+				  .collect(toMap(Function.identity(), String::length, (i1, i2) -> i1));
+		
+		System.out.println(result4);
+
+		List<String> result5 = givenList.stream()
+				  .collect(collectingAndThen(toList(), ImmutableList::copyOf));
+		
+		System.out.println(givenList.stream()
+				  .collect(joining(" ")));
+		
+		
+		
+	}
+
+}
---
+++ src/collection/FunctionalInterfaceExamples.java
@@ -0,0 +1,82 @@
+package collection;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FunctionalInterfaceExamples {
+
+	public static void main (String [] args)
+	{
+		Map<String, Integer> nameMap = new HashMap<>();
+		Integer value = nameMap.computeIfAbsent("John", s -> s.length());
+		Integer value1 = nameMap.computeIfAbsent("John", String::length);
+
+		System.out.println(value+","+value1);
+
+		Function<Integer, String> intToString = Object::toString;
+		Function<String, String> quote = s -> "'" + s + "'";
+
+		Function<Integer, String> quoteIntToString = quote.compose(intToString);
+
+		System.out.println(quoteIntToString.apply(5));
+
+		//BiFunction
+		Map<String, Integer> salaries = new HashMap<>();
+		salaries.put("John", 40000);
+		salaries.put("Freddy", 30000);
+		salaries.put("Samuel", 50000);
+
+		salaries.replaceAll((name, oldValue) -> 
+		name.equals("Freddy") ? oldValue : oldValue + 10000);
+		System.out.println(salaries);
+
+		//Supliers
+		/*
+		 * The Supplier functional interface is yet another Function specialization that does not take any
+		 *  arguments. It is typically used for lazy generation of values. For instance, let’s define a 
+		 *  function that squares a double value. It will receive not a value itself, but a Supplier of this
+		 *   value:
+		 * */
+		
+		//Consumer
+		/*
+		 As opposed to the Supplier, the Consumer accepts a generified argument and returns nothing. It is a
+		 function that is representing side effects.
+		 * */
+		List<String> names = Arrays.asList("John", "Freddy", "Samuel");
+		names.forEach(name -> System.out.println("Hello, " + name));
+		
+		//BiConsumer
+		Map<String, Integer> ages = new HashMap<>();
+		ages.put("John", 25);
+		ages.put("Freddy", 24);
+		ages.put("Samuel", 30);
+		 
+		ages.forEach((name, age) -> System.out.println(name + " is " + age + " years old"));
+		
+		//Predicaes
+		 
+		List<String> namesWithF = names.stream()
+		  .filter(name -> name.startsWith("F"))
+		  .collect(Collectors.toList());
+		System.out.println(namesWithF);
+		
+		//Operator
+		//Unary
+		names.replaceAll(name -> name.toUpperCase());
+		System.out.println(names);
+		//binary
+		List<Integer> values = Arrays.asList(3, 5, 8, 9, 12);
+		 
+		int sum = values.stream()
+		  .reduce(0, (i1, i2) -> i1 + i2);
+		
+		System.out.println(sum);
+		
+		
+	}
+}
